9d77f235a8f13aa277f1ee7bb1fc65d8468a92b3
SimarLar <simakumariktg147@gmail.com>
2026-04-29 21:52:18 +0630

    Flask frontend and Fastapi backend

---
+++ api.py
@@ -0,0 +1,77 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+
+app = FastAPI()
+
+# Load model + preprocessor once
+model = joblib.load("artifacts/model.pkl")
+preprocessor = joblib.load("artifacts/preprocessor.pkl")
+
+class CustomerData(BaseModel):
+    Gender: str
+    Senior_Citizen: str
+    Partner: str
+    Dependents: str
+    Tenure_Months: int
+    Phone_Service: str
+    Multiple_Lines: str
+    Internet_Service: str
+    Online_Security: str
+    Online_Backup: str
+    Device_Protection: str
+    Tech_Support: str
+    Streaming_TV: str
+    Streaming_Movies: str
+    Contract: str
+    Paperless_Billing: str
+    Payment_Method: str
+    Monthly_Charges: float
+    Total_Charges: float
+    CLTV: int
+
+@app.get("/")
+def health_check():
+    return {"message": "FastAPI is running"}
+
+@app.post("/predict")
+def predict(data: CustomerData):
+    
+    input_dict = {
+        "Gender": data.Gender,
+        "Senior Citizen": data.Senior_Citizen,
+        "Partner": data.Partner,
+        "Dependents": data.Dependents,
+        "Tenure Months": data.Tenure_Months,
+        "Phone Service": data.Phone_Service,
+        "Multiple Lines": data.Multiple_Lines,
+        "Internet Service": data.Internet_Service,
+        "Online Security": data.Online_Security,
+        "Online Backup": data.Online_Backup,
+        "Device Protection": data.Device_Protection,
+        "Tech Support": data.Tech_Support,
+        "Streaming TV": data.Streaming_TV,
+        "Streaming Movies": data.Streaming_Movies,
+        "Contract": data.Contract,
+        "Paperless Billing": data.Paperless_Billing,
+        "Payment Method": data.Payment_Method,
+        "Monthly Charges": data.Monthly_Charges,
+        "Total Charges": data.Total_Charges,
+        "CLTV": data.CLTV,
+        
+    }
+    input_df = pd.DataFrame([input_dict])
+    transformed = preprocessor.transform(input_df)
+    prediction = model.predict(transformed)[0]
+    probability = model.predict_proba(transformed)[0][1]
+    
+    result = "Customer Will Churn " if prediction == 1 else "Customer Will Not Churn"
+    probability = f"{round(float(probability),4) * 100}%"
+    return {
+        "prediction": result,
+        "probability": probability
+        
+        
+    }
+    
--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask, request, render_template
-import numpy as np
-import joblib
-import pandas as pd
-import joblib
+import requests
+
 
 app = Flask(__name__)
 
-# Load artifacts
-model = joblib.load("artifacts/model.pkl")
-preprocessor = joblib.load("artifacts/preprocessor.pkl")
 
 # Home Route
 @app.route('/')
@@ -16,31 +11,48 @@ def home():
     return render_template('index.html')
 
 # Predict Route
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        # Get form data
-        data = request.form.to_dict()
-        
-        # Convert to DataFrame
-        input_df = pd.DataFrame([data])
-        
-        # Preprocess input
-        transformed_data = preprocessor.transform(input_df)
-        
-        # Make prediction
-        prediction = model.predict(transformed_data)[0]
-        probability = model.predict_proba(transformed_data)[0][1]
-        
-        result = "Customer will churn" if prediction == 1 else "Customer will not churn"
-        
-        return render_template('index.html',
-                               prediction_test = result,
-                               probability=round(probability,2))
     
-    except Exception as e:
-        return str(e)
+    form_data = request.form.to_dict()
+    payload = {
+        "Gender": form_data["Gender"],
+        "Senior_Citizen": form_data["Senior Citizen"],
+        "Partner": form_data["Partner"],
+        "Dependents": form_data["Dependents"],
+        "Tenure_Months": int(form_data["Tenure Months"]),
+        "Phone_Service": form_data["Phone Service"],
+        "Multiple_Lines": form_data["Multiple Lines"],
+        "Internet_Service": form_data["Internet Service"],
+        "Online_Security": form_data["Online Security"],
+        "Online_Backup": form_data["Online Backup"],
+        "Device_Protection": form_data["Device Protection"],
+        "Tech_Support": form_data["Tech Support"],
+        "Streaming_TV": form_data["Streaming TV"],
+        "Streaming_Movies": form_data["Streaming Movies"],
+        "Contract": form_data["Contract"],
+        "Paperless_Billing": form_data["Paperless Billing"],
+        "Payment_Method": form_data["Payment Method"],
+        "Monthly_Charges": float(form_data["Monthly Charges"]),
+        "Total_Charges": float(form_data["Total Charges"]),
+        "CLTV": int(form_data["CLTV"])
+    }
 
+    response = requests.post(
+        "http://127.0.0.1:8000/predict",
+        json=payload)
+    
+    if response.status_code == 200:
+        prediction_result = response.json()
+
+        return render_template(
+        'index.html',
+        prediction_text=prediction_result["prediction"],
+        probability=prediction_result["probability"]
+      )
+
+    else:
+        return f"FastAPI Error: {response.text}"
 
 if __name__ == "__main__":
     app.run(debug=True)
